187c2fdad9e58f0f6e7a8f6c52b77dcb2d752819
Sophia Althammer <salthamm@troubadix.ifs.tuwien.ac.at>
2022-03-29 10:49:46 +0200

    added poolout to train

---
+++ preprocessing/poolout_to_train_bertpli.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#conda activate bert-pli-env
+python coliee22_poolout_to_train.py --run_before_poolout  /newstorage5/salthamm/coliee22/task1/train/run_for_bertpli_top1_15_0.json --run_after_poolout  /newstorage5/salthamm/coliee22/task1/bertpli/poolout_output/train/poolout_bertorg_top1_15_0.json
+wait
+python coliee22_poolout_to_train.py --run_before_poolout  /newstorage5/salthamm/coliee22/task1/train/run_for_bertpli_top1_15_1.json --run_after_poolout  /newstorage5/salthamm/coliee22/task1/bertpli/poolout_output/train/poolout_bertorg_top1_15_1.json
+wait
+python coliee22_poolout_to_train.py --run_before_poolout  /newstorage5/salthamm/coliee22/task1/train/run_for_bertpli_top1_15_2.json --run_after_poolout  /newstorage5/salthamm/coliee22/task1/bertpli/poolout_output/train/poolout_bertorg_top1_15_2.json
+wait
+python coliee22_poolout_to_train.py --run_before_poolout  /newstorage5/salthamm/coliee22/task1/train/run_for_bertpli_top1_15_3.json --run_after_poolout  /newstorage5/salthamm/coliee22/task1/bertpli/poolout_output/train/poolout_bertorg_top1_15_3.json
+wait
