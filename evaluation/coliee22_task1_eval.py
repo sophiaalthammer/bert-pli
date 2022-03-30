476bcf1d8e99c6a37c2d7e5cf3d62956d5b8ff90
Sophia Althammer <sophia.althammer@gmail.com>
2022-03-30 19:16:24 +0200

    added evaluation for coliee22

--- evaluation/coliee22_task1_eval.py
+++ evaluation/coliee22_task1_eval.py
@@ -24,7 +24,7 @@ def read_label_file(label_file):
     return labels
 
 
-def read_predictions(pred_file):
+def read_predictions(pred_file, score=False):
     with open(pred_file, 'r') as f:
         lines = json.load(f)
 
@@ -32,7 +32,35 @@ def read_predictions(pred_file):
     for line in lines:
         query_id = line[0].split('_')[0]
         doc_id = line[0].split('_')[1]
-        label = np.argmax(line[1])
+
+        # take the argmax as label
+        # take the score of the relevant dimension as label
+        if not score:
+            label = int(np.argmax(line[1]))
+        else:
+            label = int(line[1][1]*10000)
+
+        if pred.get(query_id):
+            pred.get(query_id).update({doc_id: label})
+        else:
+            pred.update({query_id: {}})
+            pred.get(query_id).update({doc_id: label})
+    return pred
+
+
+def read_predictions_json(pred_file, score=False):
+    with open(pred_file, 'r') as reader:
+        content = reader.read().splitlines()
+        predictions = [ast.literal_eval(file) for file in content]
+
+    pred = {}
+    for prediction in predictions:
+        query_id = prediction.get('guid').split('_')[0]
+        doc_id = prediction.get('guid').split('_')[1]
+        if not score:
+            label = int(np.argmax(prediction.get('res')))
+        else:
+            label = int(prediction.get('res')[1]*10000)
 
         if pred.get(query_id):
             pred.get(query_id).update({doc_id: label})
@@ -44,8 +72,7 @@ def read_predictions(pred_file):
 
 def ranking_eval(qrels, run, output_dir, output_file= 'eval_bm25_aggregate_overlap.txt'):
     # trec eval
-    evaluator = pytrec_eval.RelevanceEvaluator(qrels,  {'P_5', 'P_10', 'recall_5', 'recall_10', 'P_1'})
-                                               #pytrec_eval.supported_measures)
+    evaluator = pytrec_eval.RelevanceEvaluator(qrels, pytrec_eval.supported_measures)
 
     results = evaluator.evaluate(run)
 
@@ -78,7 +105,67 @@ def ranking_eval(qrels, run, output_dir, output_file= 'eval_bm25_aggregate_overl
                         [query_measures[measure]
                          for query_measures in results.values()])) + '\n')
 
+def run_to_df(labels, column_names):
+    samples = []
+    for query_id, value in labels.items():
+        for doc_id, label in value.items():
+            samples.append((query_id, doc_id, label))
 
+    df_labels = pd.DataFrame(samples, columns=column_names)
+    return df_labels
+
+
+def eval_classification(labels, predictions, output_dir):
+    df_labels = run_to_df(labels, {'query_id', 'doc_id', 'label'})
+    df_pred = run_to_df(predictions, {'query_id', 'doc_id', 'prediction'})
+
+    assert set(df_labels['query_id']) == set(df_pred['query_id'])
+
+    # join outer
+    df_joined = pd.merge(df_labels, df_pred, how='outer', on=['query_id', 'doc_id'])
+    df_joined = df_joined.fillna(0)
+
+    # manual check if the precision, recall and f1 score of the sklearn binary classification package (for relevant class 1)
+    #     return the same evaluation metrics as when they are computed manually according to the formula on
+    #     https://sites.ualberta.ca/~rabelo/COLIEE2020/
+
+    label_list = list(df_joined['label'])
+    pred_list = list(df_joined['prediction'])
+    df = df_joined
+
+    df_only_rel = df[df['label'] == 1]
+    try:
+        no_correctly_retrieved = df_only_rel['prediction'].value_counts()[1]
+    except:
+        no_correctly_retrieved = 0
+
+    no_relevant = len(df_only_rel)
+    try:
+        df_only_rel_pred = df[df['prediction'] == 1]
+        no_retrieved = len(df_only_rel_pred)
+    except:
+        no_retrieved = 0
+
+    if no_retrieved != 0:
+        precision = no_correctly_retrieved / no_retrieved
+    else:
+        precision = 0
+    recall = no_correctly_retrieved / no_relevant
+    if precision + recall != 0:
+        f1 = (2 * precision * recall) / (precision + recall)
+    else:
+        f1 = 0
+
+    with open(os.path.join(output_dir,'eval_classification.txt'), 'w+') as output:
+        output.write(classification_report(label_list, pred_list) + '\n')
+        output.write("Precision (for [0,1] class): {} \n".format(precision_score(label_list, pred_list, labels=[0, 1], average = None,
+                                                                  pos_label=1)))
+        output.write("Recall (for [0,1] class): {}\n".format(recall_score(label_list, pred_list, average= None)))
+        output.write("F1 score (for [0,1] class): {} \n".format(f1_score(label_list, pred_list, average=None), end='\n\n'))
+
+        output.write("Manually calculated Precision: {} \n".format(precision))
+        output.write("Manually calculated Recall: {}\n".format(recall))
+        output.write("Manually calculated F1 score: {} \n".format(f1, end='\n\n'))
 
 
 if __name__ == "__main__":
@@ -98,15 +185,17 @@ if __name__ == "__main__":
 
     label_file = '/mnt/c/Users/salthamm/Documents/phd/data/coliee2021/task1/test/task1_test_labels_2021.json'
     pred_file = '/mnt/c/Users/salthamm/Documents/phd/data/coliee2022/task1/bertpli/output/' \
-                'val_output/model_coliee22_attengru/output_val_merged.json'
-
+                'val_output/model_coliee22_attenlstm_pluspos/output_val_merged.json'
 
     labels = read_label_file(label_file)
-    predictions = read_predictions(pred_file)
-    output_dir = '/'.join(pred_file.split('/')[:-1]) + 'eval.txt'
+    if 'lstm' in pred_file:
+        predictions = read_predictions_json(pred_file, score=False)
+    else:
+        predictions = read_predictions(pred_file, score=False)
+    output_dir = '/'.join(pred_file.split('/')[:-1])
 
     ranking_eval(labels, predictions, output_dir, 'eval.txt')
-
+    eval_classification(labels, predictions, output_dir)
 
 
 
